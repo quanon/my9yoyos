15ead93215ae92a5348948aa73d41e49bb3cba25
QUANON <quanon86@gmail.com>
2026-03-06 20:17:25 +0900

    Remove `SharePage.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
-import SharePage from './pages/SharePage'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/share/:shareId" element={<SharePage />} />
       </Routes>
     </BrowserRouter>
   )
